9481dc378428febd508126a12d6ebb7cd1b058cc
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-14 17:59:46 +0300

    ProfileCell State Implementation

---
+++ src/Component/ProfileCell/IProfileCellProps.ts
@@ -0,0 +1,5 @@
+import { UserType } from '../../API/User/Type/UserType';
+
+export interface IProfileCellProps {
+  User: UserType;
+}
--- src/Component/ProfileCell/ProfileCell.tsx
+++ src/Component/ProfileCell/ProfileCell.tsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { Icon28UserOutgoingOutline } from '@vkontakte/icons';
 import './ProfileCell.scss';
+import { IProfileCellProps } from './IProfileCellProps';
 
-class ProfileCell extends Component {
+class ProfileCell extends Component<IProfileCellProps> {
   render() {
     return (
       <div className='PageMargin ProfileCell'>
         <div className='ProfileCell-Block'>
-          <img src='ES9P2Ws9UK8.jpg' />
+          <img src={this.props.User.avatar_src} />
           <div className='ProfileCell-Info'>
-            <span className='ProfileCell-Info-Name'>Ефим Беляков</span>
+            <span className='ProfileCell-Info-Name'>{`${this.props.User.first_name} ${this.props.User.last_name}`}</span>
             <span className='ProfileCell-Info-Level'>Уровень 1</span>
           </div>
         </div>
