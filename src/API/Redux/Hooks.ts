db62c51ea8d18cb6f69847aa60e3cfbedb060ab7
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-14 18:05:18 +0300

    Adding hooks types

--- src/API/Redux/Hooks.ts
+++ src/API/Redux/Hooks.ts
@@ -1,15 +1,18 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from './Store';
-import { Dispatch } from 'redux';
+import { AnyAction} from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const mapStateToProps = (state: RootState) => {
-  return { ...state };
+  return {
+    storeState: state,
+  };
 };
 
-export const mapDispatchToProps = (dispatch: Dispatch) => {
+export const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, void, AnyAction>) => {
   return {
     dispatch: dispatch,
   };
