2fa6165071caf73cb9dd9f06def71b0656fdc57f
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-14 18:29:37 +0300

    Room card props implementation

---
+++ src/Component/RoomCard/IRoomCardProps.ts
@@ -0,0 +1,5 @@
+import { RoomType } from '../../API/Room/Type/RoomType';
+
+export interface IRoomCardProps {
+  Room: RoomType;
+}
--- src/Component/RoomCard/RoomCard.tsx
+++ src/Component/RoomCard/RoomCard.tsx
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import './RoomCard.scss';
 import { UsersStack } from '@vkontakte/vkui';
 import { Icon24ArrowRightOutline } from '@vkontakte/icons';
+import { IRoomCardProps } from './IRoomCardProps';
 
-class RoomCard extends Component {
+class RoomCard extends Component<IRoomCardProps> {
   render() {
     return (
       <div className='PageMargin RoomCard'>
         <div className='RoomCard-Block'>
-          <span className='RoomCard-Block-Name'>Городская</span>
-          <span className='RoomCard-Block-Number'>#1231231</span>
+          <span className='RoomCard-Block-Name'>{this.props.Room.type}</span>
+          <span className='RoomCard-Block-Number'>#{this.props.Room.id}</span>
         </div>
         <div className='RoomCard-Block'>
           <div className='RoomCard-Block-Player'>
             <UsersStack photos={['ES9P2Ws9UK8.jpg']} />
-            <span>5/12 игроков</span>
+            <span>5/{this.props.Room.size} игроков</span>
           </div>
 
           <Icon24ArrowRightOutline className='RoomCard-Block-Icon' />
