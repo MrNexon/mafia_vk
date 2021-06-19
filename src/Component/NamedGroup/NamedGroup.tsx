9afc49ccbf95043d5766e9b440808d1e1bb65bd2
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-19 11:58:55 +0300

    WIP: UI Fix

--- src/Component/NamedGroup/NamedGroup.tsx
+++ src/Component/NamedGroup/NamedGroup.tsx
@@ -1,8 +1,10 @@
+import classnames from 'classnames';
 import React, { Component } from 'react';
 import './NamedGroup.scss';
 
 interface INamedGroupProps {
-  header: string;
+  className?: string;
+  header?: string;
   subheader?: string;
 }
 
@@ -10,11 +12,13 @@ class NamedGroup extends Component<INamedGroupProps> {
   render() {
     const { children, header, subheader } = this.props;
     return (
-      <div className='NamedGroup'>
-        <div className='PageMargin NamedGroup-Text'>
-          <span className='NamedGroup-Header'>{header}</span>
-          {subheader && <span className='NamedGroup-Subheader'>{subheader}</span>}
-        </div>
+      <div className={classnames('NamedGroup', this.props.className)}>
+        {header && (
+          <div className='PageMargin NamedGroup-Text'>
+            <span className='NamedGroup-Header'>{header}</span>
+            {subheader && <span className='NamedGroup-Subheader'>{subheader}</span>}
+          </div>
+        )}
         <div className='NamedGroup-Wrapper'>{children}</div>
       </div>
     );
--- src/Page/CreateRoom/CreateRoom.tsx
+++ src/Page/CreateRoom/CreateRoom.tsx
@@ -1,74 +1,104 @@
 import React, { Component } from 'react';
-import { Panel, PanelHeader, Select, View } from '@vkontakte/vkui';
-import { Icon28LockOpenOutline, Icon28Users3Outline } from '@vkontakte/icons';
+import {
+  Button,
+  Checkbox,
+  HorizontalScroll,
+  Input,
+  ModalPage,
+  ModalPageHeader,
+  PanelHeaderButton,
+  Select,
+  SizeType,
+  SliderSwitch,
+} from '@vkontakte/vkui';
+import { Icon24Dismiss, Icon28LockOpenOutline } from '@vkontakte/icons';
 
 import ContentWrapper from '../../Component/ContentWrapper/ContentWrapper';
 
 import AvailabilityCard from '../../Component/AvailabilityCard/AvailabilityCard';
-import IRootProps from '../IRootProps';
 import './CreateRoom.scss';
+import NamedGroup from '../../Component/NamedGroup/NamedGroup';
+import { connect } from 'react-redux';
+import { mapStateToProps } from '../../API/Redux/Hooks';
+import ScrollWrapper from '../../Component/ScrollWrapper/ScrollWrapper';
+import RoleCard from '../../Component/RoleCard/RoleCard';
 
-class CreateRoom extends Component<IRootProps> {
+class CreateRoom extends Component<{
+  id: string;
+}> {
   render() {
     const { id } = this.props;
     return (
-      <View activePanel='main' id={id}>
-        <Panel id='main'>
-          <PanelHeader separator={false}>Создание комнаты</PanelHeader>
-          <ContentWrapper>
-            <div className='PageMargin AvailabilityRow'>
-              <AvailabilityCard icon={<Icon28LockOpenOutline />} text='Открытая' color='basic-orange' selected />
-              <AvailabilityCard icon={<Icon28Users3Outline />} text='Друзья' color='bright-blue' />
-              <AvailabilityCard icon={<Icon28LockOpenOutline />} text='Открытая' color='basic-orange' />
-            </div>
+      <ModalPage
+        dynamicContentHeight
+        settlingHeight={90}
+        id={id}
+        header={
+          <ModalPageHeader
+            right={
+              <PanelHeaderButton>
+                <Icon24Dismiss />
+              </PanelHeaderButton>
+            }>
+            Создание комнаты
+          </ModalPageHeader>
+        }>
+        <ContentWrapper>
+          <div className='PageMargin AvailabilityRow'>
+            <AvailabilityCard icon={<Icon28LockOpenOutline />} text='Открытая' color='basic-orange' selected />
+            {/* <AvailabilityCard icon={<Icon28Users3Outline />} text='Друзья' color='bright-blue' />
+            <AvailabilityCard icon={<Icon28LockOpenOutline />} text='Открытая' color='basic-orange' />*/}
+          </div>
+          <NamedGroup className='PageMargin'>
             <Select
-              className='PageMargin'
-              placeholder='Выберите пол'
+              placeholder='Количество игроков'
+              options={Array(12)
+                .fill(0)
+                .map((value, index) => {
+                  return {
+                    value: index + 5,
+                    label: `${index + 5} игроков`,
+                  };
+                })}
+            />
+          </NamedGroup>
+          <NamedGroup className='PageMargin'>
+            <SliderSwitch
+              className='SliderSwitch'
               options={[
                 {
-                  value: '0',
-                  label: 'Мужской',
+                  name: 'Групповой звонок',
+                  value: 'group_call',
                 },
                 {
-                  value: '1',
-                  label: 'Женский',
+                  name: 'Текстовый чат',
+                  value: 'text_chat',
                 },
               ]}
             />
-            {/* <ProfileCell />
-            <ExtraCard
-              header='Твои друзья уже в игре!'
-              subheader='Подключайся к комнате'
-              type='info'
-              icon={<Icon28PlaceOutline />}
-            />
-            <NamedGroup header='Играть'>
-              <HorizontalScroll>
-                <ScrollWrapper>
-                  <Card
-                    header='Городская мафия'
-                    subheader='Мафия, дон, комиссар и дополнительные роли'
-                    color='basic-cyan'
-                  />
-                  <Card
-                    header='Классическая мафия'
-                    subheader='Мафия, дон, комиссар и ничего лишнего'
-                    color='basic-brown'
-                  />
-                  <Card header='Турнирная мафия' subheader='Скоро' color='light-purple' />
-                </ScrollWrapper>
-              </HorizontalScroll>
-            </NamedGroup>
-            <NamedGroup header='Ожидают игры'>
-              <RoomCard />
-              <RoomCard />
-              <RoomCard />
-            </NamedGroup> */}
-          </ContentWrapper>
-        </Panel>
-      </View>
+            <Input type='text' placeholder='Ссылка' />
+          </NamedGroup>
+          <NamedGroup header='Роли' subheader='2 из 3 выбрано'>
+            <HorizontalScroll>
+              <ScrollWrapper>
+                <RoleCard />
+                <RoleCard />
+                <RoleCard />
+                <RoleCard />
+              </ScrollWrapper>
+            </HorizontalScroll>
+          </NamedGroup>
+          <NamedGroup className='PageMargin'>
+            <Checkbox>Делиться геопозицией</Checkbox>
+            <Checkbox>Речь после смерти</Checkbox>
+          </NamedGroup>
+          <Button size='l' className='PageMargin'>
+            Создать игру
+          </Button>
+        </ContentWrapper>
+      </ModalPage>
     );
   }
 }
 
-export default CreateRoom;
+export default connect(mapStateToProps)(CreateRoom);
