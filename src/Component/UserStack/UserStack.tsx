4377242bc826c51a1c018c71e9e022fc9738b72c
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-25 12:39:05 +0300

    Added user restriction to UserStack

--- src/Component/UserStack/UserStack.tsx
+++ src/Component/UserStack/UserStack.tsx
@@ -9,9 +9,9 @@ class UserStack extends Component<IUserStack> {
     return (
       <div className='UserStack' data-size={this.props.size}>
         <div className='UserStack-Wrapper'>
-          {this.props.avatars.map((avatar) => (
-            <img src={avatar} />
-          ))}
+          {this.props.avatars.map((avatar, index) => {
+            if (index < 5) return <img src={avatar} alt='Player Avatar' />;
+          })}
         </div>
         {typeof this.props.children === 'string' && this.props.size === 's' && (
           <Text level={'2'}>{this.props.children}</Text>
