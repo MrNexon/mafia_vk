021eb762e24347421ac4caee85a5d7aaa3d5ff97
Efim Belyakov <belyakov.efim@gmail.com>
2021-06-25 16:46:17 +0300

    RoomCard changed according to documentation

--- src/Component/RoomCard/RoomCard.tsx
+++ src/Component/RoomCard/RoomCard.tsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import './RoomCard.scss';
-import { UsersStack } from '@vkontakte/vkui';
 import { Icon24ArrowRightOutline } from '@vkontakte/icons';
 import { IRoomCardProps } from './IRoomCardProps';
+import UserStack from '../UserStack/UserStack';
+import Text from '../Typography/Text/Text';
 
 class RoomCard extends Component<IRoomCardProps> {
   render() {
     return (
-      <div className='PageMargin RoomCard'>
-        <div className='RoomCard-Block'>
-          <span className='RoomCard-Block-Name'>{this.props.Room.RoomType.name.split(' ')[0]}</span>
-          <span className='RoomCard-Block-Number'>#{this.props.Room.id}</span>
-        </div>
-        <div className='RoomCard-Block'>
-          <div className='RoomCard-Block-Player'>
-            <UsersStack photos={this.props.Room.RoomUser.map((roomUser) => roomUser.User.avatar_src)} />
-            <span>
-              {this.props.Room.RoomUser.length}/{this.props.Room.size} игроков
-            </span>
+      <div className='RoomCard'>
+        <div className='RoomCard-Wrapper'>
+          <div className='RoomCard-Wrapper-Block'>
+            <Text level={'1'}>{this.props.Room.RoomType.name.split(' ')[0]}</Text>
+            <Text level={'2'} className={'RoomCard-Wrapper-Block-Number'}>{`#${this.props.Room.id}`}</Text>
           </div>
+          <div className='RoomCard-Wrapper-Block'>
+            <UserStack size={'s'} avatars={this.props.Room.RoomUser.map((roomUser) => roomUser.User.avatar_src)}>
+              {this.props.Room.RoomUser.length > 0
+                ? `${this.props.Room.RoomUser.length} / ${this.props.Room.size} игроков`
+                : 'Нет игроков'}
+            </UserStack>
 
-          <Icon24ArrowRightOutline className='RoomCard-Block-Icon' />
+            <Icon24ArrowRightOutline className='RoomCard-Wrapper-Block-Icon' />
+          </div>
         </div>
       </div>
     );
